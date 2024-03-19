241fcf38c37532249c8305933d37657f72620f25
Erik Werther <erik.werther@gmail.com>
2024-03-19 13:50:58 +0100

    kod

--- GritAcademyAPII/src/main/java/com/GritAcademyAPII/Entitys/Courses.java
+++ GritAcademyAPII/src/main/java/com/GritAcademyAPII/Entitys/Courses.java
@@ -1,7 +1,37 @@
 package com.GritAcademyAPII.Entitys;
 
-import jakarta.persistence.Entity;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity(name = "courses")
+@Table(name = "courses")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Courses {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Nullable
+    @Column(name = "description")
+    private  String description;
+
+    @Column(name = "yhp")
+    private Integer yhp;
+
+    @OneToMany(mappedBy = "course", fetch = FetchType.LAZY)
+    private Set<StudentCourses> students = new HashSet<>();
 }
--- GritAcademyAPII/src/main/java/com/GritAcademyAPII/Entitys/StudentCourses.java
+++ GritAcademyAPII/src/main/java/com/GritAcademyAPII/Entitys/StudentCourses.java
@@ -1,4 +1,28 @@
 package com.GritAcademyAPII.Entitys;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "studentcourses")
+@Table(name = "studentcourses")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class StudentCourses {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "student_id")
+    Students students;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "course_id")
+    Courses courses;
 }
