88133e9d4818e15c2a1a899c79c8eaa5f2e88975
Erik Werther <erik.werther@gmail.com>
2024-03-20 18:58:08 +0100

    Update StudentsDTO.java

--- GritAcademyAPII/src/main/java/com/GritAcademyAPII/Students/StudentsDTO.java
+++ GritAcademyAPII/src/main/java/com/GritAcademyAPII/Students/StudentsDTO.java
@@ -7,9 +7,15 @@ import java.util.List;
 
 @Data
 public class StudentsDTO {
+
     private Long id;
+
     private String fName;
+
     private String lName;
+
     private String town;
+
     private List<CoursesDTO> courses;
+
 }
